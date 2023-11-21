d305713b356bcd826a8a971806d4e68a4c4b8f05
unknown <abdulsamirana27@gmail.com>
2023-11-21 15:05:12 +0500

    minor changes and fixes

--- src/app/modules/teamManagment/teamManagment.module.ts
+++ src/app/modules/teamManagment/teamManagment.module.ts
@@ -1,16 +1,15 @@
-import { LOCALE_ID, NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
+
 import { TeamManagmentRoutingModule } from "./teamManagment-routing.module";
 import { TeamManagmentComponent } from "./teamManagment/teamManagment.component";
 import { SharedModule } from "../shared/shared.module";
 import { AddUserComponent } from "./add-user/add-user.component";
+import { LOCALE_ID, NgModule } from "@angular/core";
 const lang = "en-US";
 @NgModule({
   declarations: [TeamManagmentComponent, 
                   AddUserComponent
   ],
   imports: [
-    CommonModule,
     TeamManagmentRoutingModule,
     SharedModule,
   ],
