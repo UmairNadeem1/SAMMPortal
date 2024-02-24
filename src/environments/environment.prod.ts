1e3ca74e84a3e9fee54b117f22d1b0e9bfb50742
Umar551 <umar.naeem551@gmail.com>
2024-02-24 12:25:15 +0500

    env setting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://sammwebbackend-env-1.eba-vqk24sk3.ap-southeast-2.elasticbeanstalk.com/api/', //ngrok
-  imageBaseUrl: "http://sammwebbackend-env-1.eba-vqk24sk3.ap-southeast-2.elasticbeanstalk.com",
-  socketUrl:"http://sammwebbackend-env-1.eba-vqk24sk3.ap-southeast-2.elasticbeanstalk.com",
+  apiUrl: 'https://api.myvyky.com/api/', //ngrok
+  imageBaseUrl: "https://api.myvyky.com",
+  socketUrl:"https://api.myvyky.com",
   stripe_publisher_key:'pk_test_51OdZfNFXrv3oUfvH0e0SIeXwoq00dQzFvXa5Qnet2n61UTvidKcNqrqjG7uOyjbs71Rdg5UYT5TOAqpnQdcIZNq200va548OfK'
 };
