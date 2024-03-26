eca5c223f7c7265a7562e79ca5dfb82df5779f05
omer551 <unaeem@visibility.one>
2024-03-26 13:18:35 +0500

    changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://91.108.120.99/api/', //ngrok
-  imageBaseUrl: "http://91.108.120.99",
-  socketUrl:"http://91.108.120.99",
+  apiUrl: 'https://api.myvyky.com/api/', //ngrok
+  imageBaseUrl: "https://api.myvyky.com",
+  socketUrl:"https://api.myvyky.com",
   stripe_publisher_key:'pk_test_51OdZfNFXrv3oUfvH0e0SIeXwoq00dQzFvXa5Qnet2n61UTvidKcNqrqjG7uOyjbs71Rdg5UYT5TOAqpnQdcIZNq200va548OfK'
 };
