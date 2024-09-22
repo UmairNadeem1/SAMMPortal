ce47a7907f8b29da37a8aad1c238505d1ff05cf2
TheAppPotion <theapppotion@gmail.com>
2024-09-23 01:01:41 +0500

    Added: Fix of outside boxes as per image

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,6 +8,12 @@ export const environment = {
   imageBaseUrl: "https://api.myvyky.com",
   socketUrl:"https://api.myvyky.com",
   stripe_publisher_key:'pk_test_51OdZfNFXrv3oUfvH0e0SIeXwoq00dQzFvXa5Qnet2n61UTvidKcNqrqjG7uOyjbs71Rdg5UYT5TOAqpnQdcIZNq200va548OfK'
+  //apiUrl: 'http://91.108.120.99:7001/api/',
+  //  imageBaseUrl: 'http://localhost:7001',
+  //socketUrl:'http://localhost:7001',
+  //stripe_publisher_key:'pk_test_51MReuYF36Pl0hd60XoXN2l5XRp6R27qdo34FR76F3yUAhPboG1Ek7uAGyIgomUOw2QMMVDtuSiPTwRyXwTrhCCxb00n5jI1u7l',
+  //\imageBaseUrl: 'http://91.108.120.99:7001/api/',
+  // apiUrl: 'http://localhost:7001/api/',
   // apiUrl: 'http://sammwebbackend-env-1.eba-vqk24sk3.ap-southeast-2.elasticbeanstalk.com/api/', //ngrok
   // imageBaseUrl: "http://sammwebbackend-env-1.eba-vqk24sk3.ap-southeast-2.elasticbeanstalk.com"
 };
